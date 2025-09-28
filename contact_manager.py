2dfbde08c6cde983c973a0ebdb4c9b85d3316446
RStuckey1 <stuckey85@gmail.com>
2025-09-28 14:05:46 -0500

    contact class

---
+++ contact_manager.py
@@ -0,0 +1,24 @@
+#Contact Manager Program (Rolladex)
+
+## Importing libraries
+import json # Handle JSON files
+import os # Handle operational system directories
+
+## Classes
+
+### Contact Class
+class Contact:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+    
+    def __str__(self):
+        return f"Name: {self.name}, Phone: {self.phone}, Email: {self.email}"
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "phone": self.phone,
+            "email": self.email
+        }
